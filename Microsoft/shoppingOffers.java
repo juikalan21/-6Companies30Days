2991360ac73332c4b93634d2ea1a7923a5fd41de
Jui Kalan <juikalan21@gmail.com>
2025-01-05 16:42:24 +0530

    dp - top down memoization

---
+++ Microsoft/shoppingOffers.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        return calculateMinCost(price, special, needs, new HashMap());
+    }
+    private int calculateMinCost(List<Integer> price, List<List<Integer>> specials, List<Integer> needs, Map<List<Integer>, Integer> dp) {
+
+        if (dp.containsKey(needs)) {
+            return dp.get(needs);
+        }
+
+        int cost = calculateCost(price, needs);
+
+        for (List<Integer> special : specials) {
+            if (isSpecialApplicable(special, needs)) {
+                List<Integer> updatedNeeds = new ArrayList(needs);
+                for (int i = 0; i < needs.size(); i++) {
+                    updatedNeeds.set(i, needs.get(i) - special.get(i));
+                }
+                int offerCost = special.get(special.size() - 1);
+                cost = Math.min(cost, offerCost + calculateMinCost(price, specials, updatedNeeds, dp));
+            }
+        }
+        dp.put(needs, cost);
+        return cost;
+    }
+
+    private int calculateCost(List<Integer> price, List<Integer> needs) {
+        int cost = 0;
+        for (int i = 0; i < needs.size(); i++) {
+            cost += needs.get(i) * price.get(i);
+        }
+        return cost;
+    }
+
+    private boolean isSpecialApplicable(List<Integer> special, List<Integer> needs) {
+        for (int i = 0; i < needs.size(); i++) {
+            if (special.get(i) > needs.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
