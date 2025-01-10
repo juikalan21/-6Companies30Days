0a0e6296be660fbcd1ace0143d54518799e0b70d
Jui Kalan <juikalan21@gmail.com>
2025-01-10 16:59:51 +0530

    queue and linkedlist

--- Amazon/rottenOranges.java
+++ Amazon/rottenOranges.java
@@ -1,16 +1,56 @@
 class Solution {
-    void matchPairs(int n, char nuts[], char bolts[]) {
-        // code here
-        char temp[]={'!','#','$','%','&','*','?','@','^'};
-        Map<Character,Integer> mm = new HashMap<>();
-        for(int i=0;i<n;i++)mm.put(nuts[i],1);
-        int j=0;
-        for(int i=0;i<9;i++){
-            if(mm.get(temp[i])!=null){
-                nuts[j]=temp[i];
-                bolts[j]=temp[i];
-                j++;
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int freshCount = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    freshCount++;
+                } else if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                }
             }
         }
+        
+        if (freshCount == 0) {
+            return 0;
+        }
+        
+        int time = 0;
+        int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean rottenThisMinute = false;
+            
+            for (int i = 0; i < size; i++) {
+                int[] rottenLoc = queue.poll();
+                int r = rottenLoc[0];
+                int c = rottenLoc[1];
+                
+                for (int[] direction : directions) {
+                    int nr = r + direction[0];
+                    int nc = c + direction[1];
+                    
+                    if (nr < 0 || nr >= n || nc < 0 || nc >= m || grid[nr][nc] != 1) {
+                        continue;
+                    }
+                    
+                    grid[nr][nc] = 2;
+                    queue.offer(new int[]{nr, nc});
+                    freshCount--;
+                    rottenThisMinute = true;
+                }
+            }
+            
+            if (rottenThisMinute) {
+                time++;
+            }
+        }
+        
+        return freshCount == 0 ? time : -1;
     }
 }
